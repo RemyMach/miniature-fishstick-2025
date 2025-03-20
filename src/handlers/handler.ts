99eb7046405c792d45ab997d970415fc6bcfd5c9
Remy MACHAVOINE <rmachavoine@scaleway.com>
2025-03-20 20:20:26 +0100

    feat: add product patch handler

--- src/handlers/handler.ts
+++ src/handlers/handler.ts
@@ -1,9 +1,10 @@
 import { Application, Request, Response } from "express"
-import { GetProductValidation } from "./validators/get-product"
+import { GetProductValidation } from "./validators/product-id"
 import { AppDataSource } from "../db/database"
 import { Product } from "../db/models/product"
 import { CreateProductValidation } from "./validators/create-product"
 import { generateValidationErrorMessage } from "./validators/generate-validation-message"
+import { ProductUpdateValidation } from "./validators/update-product"
 
 export const initHandlers = (app: Application) => {
     app.get("/ping", (req: Request, res: Response) => {
@@ -57,6 +58,34 @@ export const initHandlers = (app: Application) => {
                 console.log(`Internal error: ${error.message}`)
             }
             res.status(500).send({ "message": "internal error" })
+        }
+    })
+
+    app.patch("/products/:id", async (req: Request, res: Response) => {
+        try {
+            const validation = ProductUpdateValidation.validate({ ...req.params, ...req.body })
+            if (validation.error) {
+                res.status(400).send(generateValidationErrorMessage(validation.error.details))
+                return
+            }
+    
+            const updateProduct = validation.value
+            const productRepository = AppDataSource.getRepository(Product)
+            const productFound = await productRepository.findOneBy({ id: updateProduct.id })
+            if (productFound === null) {
+                res.status(404).send({ "error": `product ${updateProduct.id} not found` })
+                return
+            }
+    
+            if (updateProduct.price) {
+                productFound.price = updateProduct.price
+            }
+    
+            const productUpdate = await productRepository.save(productFound)
+            res.status(200).send(productUpdate)
+        } catch (error) {
+            console.log(error)
+            res.status(500).send({ error: "Internal error" })
         }
     })
 }
