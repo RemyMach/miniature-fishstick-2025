5293072a3f26a7647724b41e4cd1757f63a03008
Remy MACHAVOINE <rmachavoine@scaleway.com>
2025-03-21 12:40:02 +0100

    feat: add create user handler

--- src/handlers/auth.ts
+++ src/handlers/auth.ts
@@ -2,6 +2,9 @@ import { Request, Response } from "express";
 import { createUserValidation } from "./validators/auth/create-user";
 import { generateValidationErrorMessage } from "./validators/generate-validation-message";
 import { hash } from "bcrypt";
+import { AppDataSource } from "../db/database";
+import { User } from "../db/models/user";
+import { QueryFailedError } from "typeorm";
 
 export const createUser = async(req: Request, res: Response) => {
     try{
@@ -13,11 +16,25 @@ export const createUser = async(req: Request, res: Response) => {
 
         const createUserRequest = validation.value
         const hashedPassword = await hash(createUserRequest.password, 10)
-        // userRepository
-        // save notre user dans la db
-        // return une 201 avec notre user créé sans le password
+        const userRepository = AppDataSource.getRepository(User)
+        const user = await userRepository.save({
+            email: createUserRequest.email,
+            password: hashedPassword
+        })
+        res.status(201).send({
+            id: user.id,
+            email: user.email,
+            created_at: user.createdAt,
+            updated_at: user.updatedAt
+        })
         
     } catch(error) {
-
+        if (error instanceof QueryFailedError && error.driverError.code === "23505") {
+            res.status(400).send({"message": "email already exist"})
+        }
+        if (error instanceof Error) {
+            console.log(error.message)
+        }
+        res.status(500).send({"message": "internal error"})
     }
 }
